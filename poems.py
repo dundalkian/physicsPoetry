f7b3206ce0f836367f13e4a4242dd5bbca96682d
Lawrence Steele <Lawrence Steele>
2017-04-26 00:31:28 -0400

    Actually have code in this one I promise

--- poems.py
+++ poems.py
@@ -4,16 +4,20 @@ import os
 import markovify
 
 testMarkov = open("test.txt" , "wb")
-testMarkov.write(bytes("Txt to write", 'UTF-8'))
+#testMarkov.write(bytes("Txt to write\n", 'UTF-8'))
+
+with open("hellaPoems.txt") as f:
+    text = f.read()
+
+text_model = markovify.NewlineText(text)
 
 
+for i in range(10):
+    #print(text_model.make_sentence(tries=140) + "\n")
+    testMarkov.write(bytes(text_model.make_sentence(tries=100) + "\n", 'UTF-8'))
 
 
-
-
-
-
-
+testMarkov.close()
 
 
 
