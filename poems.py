a0541c7f58862e0b10d837c3c07136c970d2f170
Lawrence Steele <Lawrence Steele>
2017-04-26 00:59:50 -0400

    Added some things, documented because Reinhardt is watching, probably

--- poems.py
+++ poems.py
@@ -2,22 +2,42 @@ import random
 import sys
 import os
 import markovify
+import time
 
-testMarkov = open("test.txt" , "wb")
-#testMarkov.write(bytes("Txt to write\n", 'UTF-8'))
+'''
+So I am 90% sure that currently it creates/erases a .txt file. It then runs the aptly
+named theScript() function which opens that .txt file to append. It then uses somebody else's
+space magic to create a markov chain model based on Emily Dickinson poems it then spits out
+poems in 10 line groupings every second. 
 
-with open("hellaPoems.txt") as f:
-    text = f.read()
+Why am I doing this I hear you ask? 
+    -answer coming with next update.
+'''
 
-text_model = markovify.NewlineText(text)
+def theScript():
+    testMarkov = open("test.txt", "a")
+    #testMarkov.write(bytes("Txt to write\n", 'UTF-8'))
+
+    with open("hellaPoems.txt") as f:
+        text = f.read()
+
+    text_model = markovify.NewlineText(text)
 
 
-for i in range(10):
-    #print(text_model.make_sentence(tries=140) + "\n")
-    testMarkov.write(bytes(text_model.make_sentence(tries=100) + "\n", 'UTF-8'))
+    for i in range(10):
+        #print(text_model.make_sentence(tries=140) + "\n")
+        testMarkov.write((text_model.make_sentence(tries=100) + "\n"))
+
+    testMarkov.write("\n")
 
 
-testMarkov.close()
+    testMarkov.close()
+
+deletePrevious = open("test.txt", "w")
+deletePrevious.close()
+while True:
+    theScript()
+    time.sleep(1)
 
 
 
