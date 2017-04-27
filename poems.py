5fdc15c01e9d8060aed7e0bd4434e1274b98584b
dundalkian <larrysteele117@gmail.com>
2017-04-27 02:16:17 -0400

    I am rapidly making this work, don't look at this until I fix it, goodnight

--- poems.py
+++ poems.py
@@ -4,6 +4,7 @@ import os
 import markovify
 import time
 import twitter
+import string
 
 #testMarkov = open("test.txt" , "wb")
 #testMarkov.write(bytes("Txt to write", 'UTF-8'))
@@ -78,6 +79,7 @@ def areWords(str1, str2):
 
 def isRhyme(str1, str2):
     if not areWords(str1, str2):
+        print('not words')
         return False  # eventually should redirect to the psuedowords, but Dickinson uses real words... so, just no.
     strPhone1 = ''  # Strings representing phonemes
     strPhone2 = ''
@@ -89,25 +91,72 @@ def isRhyme(str1, str2):
             if str2 in lines:
                 strPhone2 = lines
     indexPhone1 = findIndex(strPhone1, '\t', 3)
+   # print(indexPhone1)
     indexPhone2 = findIndex(strPhone2, '\t', 3)
+   # print(indexPhone2)
 
     #temp takes in phonemes one at a time and works backwards, (gets OW, shit happens, gets K...)
-    temp1 = strPhone1[:indexPhone1]
-    temp2 =
+    temp1 = strPhone1[findIndexReverse(strPhone1, '.', indexPhone1):indexPhone1]
+    temp2 = strPhone2[findIndexReverse(strPhone2, '.', indexPhone2):indexPhone2]
+    if temp1 != temp2:
+        print("not the same")
+        return False
+    if isVowel(temp1):
+        temp1 = strPhone1[findIndexReverse(strPhone1, '.', findIndexReverse(strPhone1, '.', indexPhone1)):findIndexReverse(strPhone1, '.', indexPhone1)]
+        temp2 = strPhone2[findIndexReverse(strPhone2, '.', findIndexReverse(strPhone2, '.', indexPhone2)):findIndexReverse(strPhone2, '.', indexPhone2)]
+        print(temp1)
+        if temp1 == '':
+            #print('hello there')
+            temp1 = '.' + strPhone1[findIndexReverse(strPhone1, '\t', findIndexReverse(strPhone1, '.', indexPhone1)) + 1:findIndexReverse(strPhone1, '.', indexPhone1)]
+            print(temp1)
+        if temp1 == '':
+            #print('General')
+            x=0
+        print(temp2)
+        if temp1 != temp2:
+            print('flase')
+            return False
+        else:
+            print('True')
+            return True
+    else:
+        temp1 = strPhone1[findIndexReverse(strPhone1, '.', indexPhone1):indexPhone1]
+        temp2 = strPhone2[findIndexReverse(strPhone2, '.', indexPhone2):indexPhone2]
+        if temp1 != temp2:
+            #print("why is it here")
+            return False
 
-def findIndexReverse(string, subString, n, start):
-    index = string.find(subString,end=start)  # end is equal to start, idk anymore, kill me soon.
-    while n > 1:
-        indexStart = string.find(subString, index + len(subString))
-        n = n - 1
+
+
+def isVowel(str):
+    Alpha = 'AEIOU'
+    for i in range(5):
+        index = str.find(Alpha[i])
+        if index != -1:
+            #print("vowel")
+            return True
+    #print("not vowel")
+    return False
+def findIndexReverse(string, subString, start):
+    i = 0
+    index = 0
+    while index != -1:
+        #print(index)
+        index = string.find(subString, index+1)
+        i += 1
+        if index >= start:
+            index = -1
+    i = i - 1
+
+    index = findIndex(string, subString, i)
     return index
 
 
 def findIndex(string, subString, n):  # Find index of nth occurence of a string within strings
     index = string.find(subString)
     while n > 1:
-        indexStart = string.find(subString, index+len(subString))
-        n = n-1
+        index = string.find(subString, index+len(subString))
+        n = n - 1
     return index
 
 
@@ -123,6 +172,62 @@ def tweetFourLinePoem(api):
 
     api.PostUpdate(tweetText)
 
+def makeTwoLines():
+    with open("hellaPoems.txt") as f:
+        corpusText = f.read()
+
+    markovModel = markovify.NewlineText(corpusText)
+    Alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+    line1 = markovModel.make_short_sentence(max_chars=34)
+    line2 = markovModel.make_short_sentence(max_chars=34)
+    print(line1)
+
+
+    indexLast1 = findIndexReverse(line1, ' ', len(line1))
+    indexLast2 = findIndexReverse(line2, ' ', len(line2))
+
+    charLast1 = line1[len(line1) - 1:len(line1)]
+    charLast2 = line2[len(line2) - 1:len(line2)]
+
+    endLine1 = len(line1)
+    endLine2 = len(line2)
+    booly1 = False
+    for i in range(len(Alphabet)):
+        booly1 = False
+        if charLast1 == Alphabet[i]:
+            booly1 = True
+
+
+
+    if booly1 == False:
+        endLine1 = endLine1 - 1
+
+    booly2 = False
+    for i in range(len(Alphabet)):
+        booly2 = False
+        if charLast1 == Alphabet[i]:
+            booly2 = True
+
+
+
+    if booly2 == False:
+        endLine2 = endLine2 - 1
+
+
+
+    last1 = line1[indexLast1+1:endLine1]
+    print(last1)
+    last2 = line2[indexLast2+1:endLine2]
+    print(last2)
+
+    if isRhyme('\t'+last1+'\t', '\t'+last2+'\t'):
+        print(line1)
+        print(line2)
+        return True
+    else:
+        print('rip')
+        return False
 
 def theScript():
     testMarkov = open("test.txt", "a")
@@ -143,13 +248,17 @@ def theScript():
 
     testMarkov.close()
 
-tweetFourLinePoem(getAPI())
-isRhyme('\tton\t', 'zuni')
+#tweetFourLinePoem(getAPI())
+isRhyme('\tbolt\t', '\tsnow\t')
+makeTwoLines()
 
-
-
-
-
+test(False)
+def test(x):
+    if x == True:
+        print('YOU DID IT')
+        return True
+    else:
+        return test(makeTwoLines())
 
 
 
